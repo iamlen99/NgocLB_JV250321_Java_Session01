c2d3ca1ced99c57b5e7aa14099ab68602b493024
iamlen99 <allblue2903@gmail.com>
2025-06-23 17:09:50 +0700

    Upload bài tập MD04 session 01 - ex2

---
+++ TPLUS_JV250321_MD04_Session01_Bai02/src/StudentInfo.java
@@ -0,0 +1,8 @@
+public class StudentInfo {
+    public static void main(String[] args) {
+        String studentName = "Lê Bảo Ngọc";
+        int studentAge = 26;
+        String studentAddress = "Nghệ An";
+        System.out.printf("Tôi tên là %s, tôi %d tuổi, hiện tại đang học tại PTIT-HCM. Quê ở %s", studentName, studentAge, studentAddress);
+    }
+}
